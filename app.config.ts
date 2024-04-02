21718235062cde1dc432775dad83c7fb67ac82f6
Filip Skoczen <philipskoczen@gmail.com>
2024-04-02 20:41:20 +0200

    Add prerender

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from '@solidjs/start/config';
 import unocss from 'unocss/vite';
 
 export default defineConfig({
-	ssr:  false,
+	ssr:    true,
+	server: {
+		prerender: { routes: ['/'] },
+	},
 	vite: {
 		plugins: [
 			unocss() as any,
